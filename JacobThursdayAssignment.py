afe51f934b12fd2fb1b9e084ca1d5a1ab3b467d2
Jacob Bancroft <jacobancroft35@gmail.com>
2020-04-24 15:13:36 -0400

    I should've named this better..

---
+++ JacobThursdayAssignment.py
@@ -0,0 +1,31 @@
+import pygame, sys, time
+from pygame.locals import *
+
+pygame.init()
+WINDOWWIDTH = 400
+WINDOWHEIGHT = 400
+windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
+pygame.display.set_caption('Animation')
+
+DOWNLEFT = 'downleft'
+DOWNRIGHT = 'downright'
+UPLEFT = 'upleft'
+UPRIGHT = 'upright'
+
+MOVESPEED = 4
+
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+b1 = {'rect':pygame.Rect(300, 80, 50, 100), 'color':RED, 'dir':UPRIGHT}
+b2 = {'rect':pygame.Rect(200, 200, 20, 20), 'color':GREEN, 'dir':UPLEFT}
+b3 = {'rect':pygame.Rect(100, 150, 60, 60), 'color':BLUE, 'dir':DOWNLEFT}
+boxes = [b1, b2, b3]
+
+windowSurface.fill(WHITE)
+for b in boxes:
+    pygame.draw.rect(windowSurface,b['color'], b['rect'])
+
+pygame.display.update()
